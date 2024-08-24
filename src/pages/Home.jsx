458fa51a0f13af594e20d36b42e68082c1a3e58c
Al Amin <alamin931761@gmail.com>
2024-08-24 12:12:54 +0600

    data fetched done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [product, setProduct] = useState([]);
   useEffect(() => {
     fetch(
-      `https://react-interview.1putym.easypanel.host/api/product?email=naim.microdeft@gmail.com`
-    ).then((res) => res.json().then((data) => console.log(data)));
+      `https://react-interview.1putym.easypanel.host/api/product?email=alamin931761@gmail.com`
+    ).then((res) => res.json().then((data) => setProduct(data)));
   }, []);
+
+  console.log(product);
+
   return (
     <div>
       <h1>Home Page</h1>
